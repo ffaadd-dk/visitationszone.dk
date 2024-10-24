c3dab7174a252cbc201f7330a8d4fdef255a12c1
Christian Panton <christian@panton.org>
2024-10-24 11:44:36 +0200

    update check not to fail if timeout

--- build/check.py
+++ build/check.py
@@ -1,18 +1,24 @@
+import sys
 import requests
 
 from bs4 import BeautifulSoup
 
-f = open("current.txt", "w")
 
 URL = "https://politi.dk/lov-og-information/visitationszoner"
 UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 
-r = requests.get(URL, headers={"User-Agent": UA})
+try:
+    r = requests.get(URL, headers={"User-Agent": UA})
+except:
+    print("Failed to connect, skipping update")
+    sys.exit(0)
 
 s = BeautifulSoup(r.text, features="html.parser")
 el = s.find("div", class_="QAndAAccordion")
 
 cards = el.find_all("div", class_="card")
+
+f = open("current.txt", "w")
 
 for card in cards:
     area = card.find("div", class_="card-header") \
